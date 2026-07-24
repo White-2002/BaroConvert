839aca75c7bc2200743444cba277f7df324c411a
White-2002 <yoosin200253@gmail.com>
2026-07-24 23:46:07 +0900

    Bump app version to v0.5.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "kr.yoosi.baroconvert"
         minSdk = 26
         targetSdk = 36
-        versionCode = 7
-        versionName = "0.5.0"
+        versionCode = 8
+        versionName = "0.5.1"
     }
 
     buildFeatures {
