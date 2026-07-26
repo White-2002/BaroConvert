be284b8ed1172f499277707257d2472b255d8f0c
White-2002 <yoosin200253@gmail.com>
2026-07-26 21:29:46 +0900

    Bump Android app to v0.7.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "kr.yoosi.baroconvert"
         minSdk = 26
         targetSdk = 36
-        versionCode = 13
-        versionName = "0.7.1"
+        versionCode = 14
+        versionName = "0.7.2"
     }
 
     buildFeatures {
